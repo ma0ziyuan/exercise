d3130f1ea6c31e31d825a2ae927f003d35882cf0
maoziyuan <maoziyuan@139.com>
2018-06-15 11:24:53 +0800

    100 doors 新增 访问门一次 和  访问门两次 的功能

--- src/main/java/com/migu/csdexercise/OneHundredDoors.java
+++ src/main/java/com/migu/csdexercise/OneHundredDoors.java
@@ -2,23 +2,43 @@ package com.migu.csdexercise;
 
 public class OneHundredDoors {
     private final int DOOR_AMOUNT = 100;
-    private final int DOOR_CLOSED_STATUS = 0;
-    private final int DOOR_OPENED_STATUS = 1;
+    public static final int DOOR_STATUS_CLOSED = 0;
+    public static final int DOOR_STATUS_OPENED = 1;
 
     private int[] doors = new int[DOOR_AMOUNT];
-
-
-    public OneHundredDoors() {
-        for (int i = 0; i < doors.length; i++) {
-            doors[i] = 0;
-        }
-    }
 
     public int[] getDoors() {
         return doors;
     }
 
-    public void setDoors(int[] doors) {
-        this.doors = doors;
+    public OneHundredDoors() {
+        for (int i = 0; i < doors.length; i++) {
+            doors[i] = DOOR_STATUS_CLOSED;
+        }
+    }
+
+    public void visitOnce() {
+        for (int i = 0; i < doors.length; i++) {
+            doors[i] = toggleDoorStatus(doors[i]);
+        }
+    }
+
+    private int toggleDoorStatus(int doorStatus) {
+        if (doorStatus == DOOR_STATUS_OPENED)
+            return DOOR_STATUS_CLOSED;
+        else
+            return DOOR_STATUS_OPENED;
+    }
+
+    public void visitTwice() {
+        for (int i = 0; i < doors.length; i++) {
+            doors[i] = toggleDoorStatus(doors[i]);
+        }
+
+        for (int i = 0; i < doors.length; i++) {
+            if (i % 2 == 0) {
+                doors[i] = toggleDoorStatus(doors[i]);
+            }
+        }
     }
 }
--- src/test/java/com/migu/csdexercise/OneHundredDoorsTest.java
+++ src/test/java/com/migu/csdexercise/OneHundredDoorsTest.java
@@ -4,26 +4,48 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class OneHundredDoorsTest {
 
     private OneHundredDoors oneHundredDoors;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         oneHundredDoors = new OneHundredDoors();
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
     }
 
     @Test
     public void testDoorsInit() {
         int[] doors = oneHundredDoors.getDoors();
-        for (int i = 0; i < doors.length; i++) {
-            assertEquals(0,doors[i]);
+        for (int doorStatus : doors) {
+            assertEquals(OneHundredDoors.DOOR_STATUS_CLOSED, doorStatus);
+        }
+    }
+
+    @Test
+    public void testDoorsVisitOnce() {
+        int[] doors = oneHundredDoors.getDoors();
+        oneHundredDoors.visitOnce();
+        for (int doorStatus : doors) {
+            assertEquals(OneHundredDoors.DOOR_STATUS_OPENED, doorStatus);
+        }
+    }
+
+    @Test
+    public void testDoorsVisitTwice() {
+        int[] doors = oneHundredDoors.getDoors();
+        oneHundredDoors.visitTwice();
+        for (int i=0;i<doors.length;i++) {
+            if (i%2 == 0) {
+                assertEquals(OneHundredDoors.DOOR_STATUS_CLOSED, doors[i]);
+            } else {
+                assertEquals(OneHundredDoors.DOOR_STATUS_OPENED, doors[i]);
+            }
         }
     }
 
