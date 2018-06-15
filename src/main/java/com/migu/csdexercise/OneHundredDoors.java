fc03a13694c6a91fab5eb15e49828c7c5b2d9985
maoziyuan <maoziyuan@139.com>
2018-06-15 11:34:22 +0800

    100 doors 重构代码，优化了访问门的实现逻辑，取消了代码冗余

--- src/main/java/com/migu/csdexercise/OneHundredDoors.java
+++ src/main/java/com/migu/csdexercise/OneHundredDoors.java
@@ -1,26 +1,24 @@
 package com.migu.csdexercise;
 
-public class OneHundredDoors {
+class OneHundredDoors {
     private final int DOOR_AMOUNT = 100;
-    public static final int DOOR_STATUS_CLOSED = 0;
-    public static final int DOOR_STATUS_OPENED = 1;
+    static final int DOOR_STATUS_CLOSED = 0;
+    static final int DOOR_STATUS_OPENED = 1;
 
     private int[] doors = new int[DOOR_AMOUNT];
 
-    public int[] getDoors() {
+    int[] getDoors() {
         return doors;
     }
 
-    public OneHundredDoors() {
+    OneHundredDoors() {
         for (int i = 0; i < doors.length; i++) {
             doors[i] = DOOR_STATUS_CLOSED;
         }
     }
 
-    public void visitOnce() {
-        for (int i = 0; i < doors.length; i++) {
-            doors[i] = toggleDoorStatus(doors[i]);
-        }
+    void visitOnce() {
+        visit(1);
     }
 
     private int toggleDoorStatus(int doorStatus) {
@@ -30,15 +28,16 @@ public class OneHundredDoors {
             return DOOR_STATUS_OPENED;
     }
 
-    public void visitTwice() {
+    private void visit(int order) {
         for (int i = 0; i < doors.length; i++) {
-            doors[i] = toggleDoorStatus(doors[i]);
-        }
-
-        for (int i = 0; i < doors.length; i++) {
-            if (i % 2 == 0) {
+            if (i % order == 0) {
                 doors[i] = toggleDoorStatus(doors[i]);
             }
         }
+    }
+
+    void visitTwice() {
+        visit(1);
+        visit(2);
     }
 }
