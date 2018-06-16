cf1a46e4305f653391dbd7b88fec50690f7f22ea
maoziyuan <maoziyuan@139.com>
2018-06-16 20:38:28 +0800

    测试上传测试覆盖报告更加简洁的写法，修复尝试

---
+++ src/test/groovy/com/migu/csdexercise/OneHundredDoorsTest.groovy
@@ -0,0 +1,28 @@
+package com.migu.csdexercise
+
+import spock.lang.Specification
+
+class OneHundredDoorsSpockTest extends Specification {
+    void setup() {
+        OneHundredDoors oneHundredDoors = new OneHundredDoors();
+    }
+
+    void cleanup() {
+    }
+
+    def "DoorsInit"() {
+
+    }
+
+    def "VisitOnce"() {
+        given: ""
+        when:""
+        then:""
+    }
+
+    def "VisitTwice"() {
+    }
+
+    def "VisitOneHundredTunes"() {
+    }
+}
