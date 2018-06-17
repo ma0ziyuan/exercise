dcbdc3053ab40f6c9e22d420298df99e3179451f
maoziyuan <maoziyuan@139.com>
2018-06-17 12:37:25 +0800

    新增Number Chains题目代码

---
+++ src/main/java/com/migu/csdexercise/numberchains/NumberChains.java
@@ -0,0 +1,13 @@
+package com.migu.csdexercise.numberchains;
+
+class NumberChains {
+    private String input;
+
+    NumberChains(String input) {
+        this.input = input;
+    }
+
+    int calChainLength() {
+        return 2;
+    }
+}
---
+++ src/test/java/com/migu/csdexercise/numberchains/NumberChainsTest.java
@@ -0,0 +1,13 @@
+package com.migu.csdexercise.numberchains;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumberChainsTest {
+    @Test
+    public void testCalChainLength() {
+        NumberChains numberChains = new NumberChains("123456789");
+        assertEquals(2,numberChains.calChainLength());
+    }
+}
